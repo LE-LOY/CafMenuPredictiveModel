27944b503b1a7839b58b6c188e67d5ef344f6acf
Franz Louis T. Cesista <franzlouiscesista@gmail.com>
2017-02-21 02:11:21 +0800

    cleaned up pearsonAnal.py

--- analysis/pearsonAnal.py
+++ analysis/pearsonAnal.py
@@ -1,49 +1,54 @@
 from scipy.stats.stats import pearsonr
 
+#  import items data
 with open('items/allItems.txt') as f:
     allItems = [line.split()[0] for line in f.readlines()]
 
+#  import weather data
 with open('weather-2015-Aug.txt') as f:
     weatherTemp = [map(float, line.split()[1:]) for line in f.readlines()]
 
-weather = [[], [], [], [], []]
-for i in range(31):
-    if i % 7 >= 2:
-        temp = weatherTemp[i]
-        for j in range(5):
-            weather[j].append(temp[j])
+def isWeekday(day):
+    if day % 7 >= 2:
+        return True
+    return False
+
+def correlates(results):
+    for factor in range(5):
+        if abs(results[factor][0]) >= 0.5 and results[factor][1] <= 0.05:
+            return True
+    return False
     
+#  prepare weather data
+weather = [[], [], [], [], []]
+for day in range(31):
+    if isWeekday(day):
+        for factor in range(5):
+            weather[factor].append(weatherTemp[day][factor])
+
+#  import item sales per day data           
 data = []
-for i in range(31):
-    with open('items/' + str(i+1) + '.txt') as f:
+for day in range(31):
+    with open('items/' + str(day + 1) + '.txt') as f:
         temp = [line.split()[:2] for line in f.readlines()]
-        data.append({temp[i][0] : temp[i][1] for i in range(len(temp))})
+        data.append({temp[day][0] : temp[day][1] for day in range(len(temp))})
         
-#print weather
-print allItems
-
 save = ''
-
 for item in allItems:
     sales = []
-    for i in range(31):
-        if i % 7 >= 2:
-            sales.append(float(data[i].get(item, "0")))
+    for day in range(31):
+        if isWeekday(day):
+            sales.append(float(data[day].get(item, "0")))
     
-    #to_include = False
-    to_include = False
-    results = [pearsonr(sales, weather[i]) for i in range(5)]
+    results = [pearsonr(sales, weather[factor]) for factor in range(5)]
     
-    for i in range(5):
-        if abs(results[i][0]) >= 0.5 and results[i][1] <= 0.05:
-            to_include = True
-    
-    if to_include:
+    if correlates(results):
         save += item + '\n'
-        for i in range(5):
-            save += str(results[i][0]) + ' ' + str(results[i][1]) + '\n'
+        for factor in range(5):
+            save += ' '.join(map(str, results[factor])) + '\n'
         save += '\n'
-    
+
+#  save results
 savefile = open('pearsonCor.txt', 'w')
 savefile.write(save)
 savefile.close()
